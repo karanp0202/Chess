e624c0ca96b4d8b268f7c413c839a014e6015d05
Karan Patil <karanp0202@gmail.com>
2022-11-02 14:30:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 var fs = require('fs');
 
 const app = express();
-const port = 80;
+const port = 3000;
 
 app.use(express.static(path.join(__dirname, "../frontend/")));
 
